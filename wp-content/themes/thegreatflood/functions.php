0edbd34686bedde478438c4b82a69c9fd1c35ed3
kylecarter <kyle.andrew.carter@gmail.com>
2018-01-09 01:37:33 -0600

    Adding updates to additional git issues.
    This addresses:
    * issue #19 - Interior page img missing styling
    * issue #21 - Responsive Layout Missing - Desktop
    * issue #22 - Responsive Layout Missing - "Laptop"
    
    Date:      Tue Jan 9 01:37:33 2018 -0600
    
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/atoms/_img.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/atoms/_typography.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/compounds/_home.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/elements/_article.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/elements/_sidebar.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/elements/_summary.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/molcules/_menus.sass
    	modified:   wp-content/themes/thegreatflood/frontend/source/sass/molcules/_navs.sass
    	modified:   wp-content/themes/thegreatflood/functions.php
    	modified:   wp-content/themes/thegreatflood/sidebar.php
    	modified:   wp-content/themes/thegreatflood/style.min.css

--- wp-content/themes/thegreatflood/functions.php
+++ wp-content/themes/thegreatflood/functions.php
@@ -13,34 +13,34 @@ if ( function_exists( 'register_sidebar' ) ) {
 }
 
 function thegreatflood_custom_logo_setup() {
-    $defaults = array(
-        'height'      => 240,
-        'width'       => 240,
-        'flex-height' => true,
-        'flex-width'  => true,
-        'header-text' => array( 'site-title', 'site-description' ),
-    );
-    add_theme_support( 'custom-logo', $defaults );
+  $defaults = array(
+    'height'      => 240,
+    'width'       => 240,
+    'flex-height' => true,
+    'flex-width'  => true,
+    'header-text' => array( 'site-title', 'site-description' ),
+  );
+  add_theme_support( 'custom-logo', $defaults );
 }
 add_action( 'after_setup_theme', 'thegreatflood_custom_logo_setup' );
 
 function thegreatflood_enqueue_scripts() {
-    wp_enqueue_style( 'style-min', get_template_directory_uri() . '/style.min.css' );
-    wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/modernizr.min.js', array(), 1, false );
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/script.min.js', array(), 1, true );
+  wp_enqueue_style( 'style-min', get_template_directory_uri() . '/style.min.css' );
+  wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/modernizr.min.js', array(), 1, false );
+  wp_enqueue_script( 'script', get_template_directory_uri() . '/script.min.js', array(), 1, true );
 }
 add_action( 'wp_enqueue_scripts', 'thegreatflood_enqueue_scripts' );
 
 function thegreatflood_excerpts_to_pages() {
-    add_post_type_support('page', 'excerpt');
+  add_post_type_support('page', 'excerpt');
 }
 add_action( 'init', 'thegreatflood_excerpts_to_pages' );
 
 function thegreatflood_menus() {
-    register_nav_menus( array(
-        'thegreatflood-connect-menu' => __( 'Connect' ),
-        'thegreatflood-project-menu' => __( 'Projects' )
-    ));
+  register_nav_menus( array(
+    'thegreatflood-connect-menu' => __( 'Connect' ),
+    'thegreatflood-project-menu' => __( 'Projects' )
+  ));
 }
 add_action( 'init', 'thegreatflood_menus' );
 
@@ -50,40 +50,15 @@ function thegreatflood_custom_menu_page_removing() {
 add_action( 'admin_menu', 'thegreatflood_custom_menu_page_removing' );
 
 function thegreatflood_edit_form_after_title() {
-    $tip = '<strong>TIP:</strong> To create a single line break use <code>SHIFT+RETURN</code>. By default, <code>RETURN</code> creates a new paragraph.';
-    echo '<p style="margin-bottom:0;">'.$tip.'</p>';
+  $tip = '<strong>TIP:</strong> To create a single line break use <code>SHIFT+RETURN</code>. By default, <code>RETURN</code> creates a new paragraph.';
+  echo '<p style="margin-bottom:0;">'.$tip.'</p>';
 }
 add_action( 'edit_form_after_title', 'thegreatflood_edit_form_after_title' );
 
-function thegreatflood_image_send_to_editor( $html, $id, $caption, $title, $align, $url, $size, $alt ) {
-   $image = wp_get_attachment_image_src( $id, 'full', FALSE );
-   if ( !$image ) return '';
-
-   $metadata = wp_get_attachment_metadata( $id );
-   $srcset = wp_calculate_image_srcset( array( $image[ 1 ], $image[ 2 ] ), $image[ 0 ], $metadata );
-
-    $classes = 'img-responsive wp-image-' . $id . ' img-' . $image[ 1 ] . '-by-' . $image[ 2 ];
-    $classes .= gettype( $size ) === 'string' ? ' img-size-' . $size : '';
-    if ( $align === 'left' ) {
-        $classes .= ' block-left';
-    } elseif ( $align === 'right' ) {
-        $classes .= ' block-right';
-    } elseif ( $align === 'center' ) {
-        $classes .= ' block-center';
-    } else {
-        $classes .= ' block-none';
-    }
-    $classes = apply_filters( 'get_image_tag_class', $classes, $id, $align, $size );
-
-    $markup = isset( $url ) && !empty( $url ) ? '<a href="' . $url . '"><figure class="img"' : '<figure class="img"';
-    $markup .= ' id="wp-image-' . $id . '">';
-    $markup .= '<img src="' . $image[ 0 ] . '" srcset="' . $srcset . '" class="' . $classes . '" alt="' . esc_attr( $alt ) . '" tile="' . esc_attr( $title ) . '" />';
-    $markup .= isset( $caption ) && !empty( $caption ) ? '<figcaption>' . $caption . '</figcaption>' : '';
-    $markup .= '</figure>';
-
-    return $markup;
+function thegreatflood_image_class( $class ){
+  return $class . ' img-responsive';
 }
-add_filter( 'image_send_to_editor', 'thegreatflood_image_send_to_editor', 10, 8 );
+add_filter( 'get_image_tag_class', 'thegreatflood_image_class' );
 
 function thegreatflood_previous_post_link( $output ) {
   return str_replace( '<a href=', '<a class="lnk lnk-pager lnk-pager-prev" href=', $output );
@@ -94,3 +69,10 @@ function thegreatflood_next_post_link( $output ) {
   return str_replace( '<a href=', '<a class="lnk lnk-pager lnk-pager-next" href=', $output );
 }
 add_filter( 'next_post_link', 'thegreatflood_next_post_link' );
+
+function thegreatflood_filter_ptags_on_images( $content ) {
+  $content = preg_replace( '/(\[caption id="[^"]*" align="[^"]*" width="[0-9]*")(\]\s*<img)/iU', '$1 class="wp-image"$2', $content );
+  $content = preg_replace( '/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '<div class="wp-image">$1$2$3</div>', $content );
+  return $content;
+}
+add_filter( 'the_content', 'thegreatflood_filter_ptags_on_images' );
--- wp-content/themes/thegreatflood/sidebar.php
+++ wp-content/themes/thegreatflood/sidebar.php
@@ -4,25 +4,25 @@ $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
 ?>
 
 <aside id="secondary" class="widget-area sidebar" role="complementary">
-    <?php if ( is_home() && is_front_page() ): ?>
-    <nav role="navigation" id="nav" class="nav nav-connect">
-        <?php if ( has_custom_logo() ): ?>
-        <figure class="logo logo-default">
-            <a href="<?php print get_site_url(); ?>" role="link"><img class="img-responsive" src="<?php print esc_url( $logo[ 0 ] ) ?>" alt="Shannon Flood, UX and UI Designer"></a>
-            <figcaption>
-                <h1 class="title"><a href="<?php print get_site_url(); ?>" role="link"><?php print get_bloginfo( 'name' ); ?></a></h1>
-                <h2 class="tag-line"><?php print get_bloginfo( 'description' ); ?></h2>
-            </figcaption>
-        </figure>
-        <?php else: ?>
+  <?php if ( is_home() && is_front_page() ): ?>
+  <nav role="navigation" id="nav" class="nav nav-connect">
+    <?php if ( has_custom_logo() ): ?>
+    <figure class="logo logo-default">
+      <a href="<?php print get_site_url(); ?>" role="link"><img class="img-responsive" src="<?php print esc_url( $logo[ 0 ] ) ?>" alt="Shannon Flood, UX and UI Designer"></a>
+      <figcaption>
         <h1 class="title"><a href="<?php print get_site_url(); ?>" role="link"><?php print get_bloginfo( 'name' ); ?></a></h1>
-        <?php endif; ?>
-        <?php wp_nav_menu( array( 'theme_location' => 'thegreatflood-connect-menu' ) ); ?>
-    </nav>
-    <?php else: ?> 
-    <nav role="navigation" id="nav" class="nav nav-projects">
-       <h1 class="title sr-only"><?php print get_bloginfo( 'name' ); ?></h1>
-       <?php wp_nav_menu( array( 'theme_location' => 'thegreatflood-project-menu' ) ); ?>
-    </nav>
+        <h2 class="tag-line"><?php print get_bloginfo( 'description' ); ?></h2>
+      </figcaption>
+    </figure>
+    <?php else: ?>
+    <h1 class="title"><a href="<?php print get_site_url(); ?>" role="link"><?php print get_bloginfo( 'name' ); ?></a></h1>
     <?php endif; ?>
+    <?php wp_nav_menu( array( 'theme_location' => 'thegreatflood-connect-menu' ) ); ?>
+  </nav>
+  <?php else: ?>
+  <nav role="navigation" id="nav" class="nav nav-projects">
+   <h1 class="title sr-only"><?php print get_bloginfo( 'name' ); ?></h1>
+   <?php wp_nav_menu( array( 'theme_location' => 'thegreatflood-project-menu' ) ); ?>
+  </nav>
+  <?php endif; ?>
 </aside>
